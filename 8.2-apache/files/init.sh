e7d13cf4f68d1face0b7797cbb627610a4386848
Tom <68326646+t0m0094@users.noreply.github.com>
2024-06-16 16:08:44 +0200

    update

--- 8.2-apache/files/init.sh
+++ 8.2-apache/files/init.sh
@@ -1,16 +1,17 @@
 #/bin/bash
 
 # Set variables
-SERVERNAMEFOLDER=${HOSTING_DOMAINNAME//./_}
+FOLDERNAME=${HOSTING_DOMAINNAME//./_}
 
-cp /etc/apache2/sites-available/template_vhost.conf /etc/apache2/sites-available/$(echo $SERVERNAMEFOLDER).conf
-sed -i 's|<ServerName>|${echo HOSTING_DOMAINNAME}|g' /etc/apache2/sites-available/$(echo $SERVERNAMEFOLDER).conf
-sed -i 's|<ServerAdmin>|${echo HOSTING_SERVERADMIN}|g' /etc/apache2/sites-available/$(echo $SERVERNAMEFOLDER).conf
-sed -i 's|<ServerNameFolder>|${echo SERVERNAMEFOLDER}|g' /etc/apache2/sites-available/$(echo $SERVERNAMEFOLDER).conf
+cp /etc/apache2/sites-available/template_vhost.conf /etc/apache2/sites-available/$(echo $FOLDERNAME).conf
+sed -i 's|<ServerName>|${echo HOSTING_DOMAINNAME}|g' /etc/apache2/sites-available/$(echo $FOLDERNAME).conf
+sed -i 's|<ServerAdmin>|${echo HOSTING_SERVERADMIN}|g' /etc/apache2/sites-available/$(echo $FOLDERNAME).conf
+sed -i 's|<FolderName>|${echo FOLDERNAME}|g' /etc/apache2/sites-available/$(echo $FOLDERNAME).conf
 
-mkdir /var/www/html/$(echo $SERVERNAMEFOLDER)
+mkdir /var/www/logs
+mkdir /var/www/html/$(echo $FOLDERNAME)
 
-ln -s /etc/apache2/sites-available/$(echo $SERVERNAMEFOLDER).conf /etc/apache2/sites-enabled/$(echo $SERVERNAMEFOLDER).conf
+ln -s /etc/apache2/sites-available/$(echo $FOLDERNAME).conf /etc/apache2/sites-enabled/$(echo $FOLDERNAME).conf
 
 # Start apache
 apache2-foreground
