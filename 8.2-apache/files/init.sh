86b13a9b58da335b07e2ffa74a77c260994b8138
Tom <68326646+t0m0094@users.noreply.github.com>
2024-06-16 15:59:23 +0200

    Update init.sh

--- 8.2-apache/files/init.sh
+++ 8.2-apache/files/init.sh
@@ -1,8 +1,12 @@
 #/bin/bash
 
 # Set variables
+SERVERNAMEFOLDER=${HOSTING_DOMAINNAME//./_}
 
-servername=${HOSTING_DOMAINNAME//./_}
+cp /etc/apache2/sites-available/template_vhost.conf /etc/apache2/sites-available/$(echo $servername).conf
+sed -i 's|<ServerName>|${echo HOSTING_DOMAINNAME}|g' /etc/apache2/conf-enabled/security.conf
+sed -i 's|<ServerAdmin>|${echo HOSTING_SERVERADMIN}|g' /etc/apache2/conf-enabled/security.conf
+sed -i 's|<ServerNameFolder>|${echo SERVERNAMEFOLDER}|g' /etc/apache2/conf-enabled/security.conf
 
 mkdir /var/www/html/$(echo $servername)
 
